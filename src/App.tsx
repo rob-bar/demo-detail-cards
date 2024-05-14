c0f5ccfa2458dba6cd6b47fe203581765ba84f8c
Robbie Bardijn <robbie.bardijn@gmail.com>
2024-05-14 17:15:31 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,203 @@
+import { styled } from "styled-components";
+
+export function App() {
+  return (
+    <DetailedWrapper>
+      <Detail>
+        <DetailImage
+          src="https://picsum.photos/seed/picsum/400/500"
+          alt="image"
+        />
+        <Content>
+          <h2>
+            Ullamco officia qui ipsum excepteur sit sint fugiat in eiusmod ut
+            nisi excepteur id aute lorem consequat aliquip laboris deserunt
+            fugiat esse voluptate quis culpa id et labore sunt veniam
+          </h2>
+          <p>
+            Anim laboris exercitation fugiat, cillum. Fugiat cillum, culpa
+            minim. Minim exercitation sint qui deserunt irure proident. Sint qui
+            deserunt irure proident ullamco esse. Deserunt irure proident
+            ullamco esse eiusmod. Proident ullamco, esse eiusmod. Eiusmod aute
+            ad nulla, id. Nulla id qui velit, nulla deserunt quis. Velit, nulla
+            deserunt quis. Quis consequat qui, minim ut. Minim ut proident
+            excepteur consectetur. Proident excepteur consectetur pariatur.
+            Consectetur pariatur exercitation lorem proident enim ut dolore.
+            Exercitation lorem proident enim ut dolore, pariatur. Enim ut dolore
+            pariatur duis enim. Dolore pariatur duis enim, fugiat non esse. Enim
+            fugiat non esse dolor. Non esse dolor, eiusmod sunt. Eiusmod sunt
+            quis, nostrud. Nostrud pariatur aliquip pariatur.
+          </p>
+        </Content>
+      </Detail>
+      <Detail>
+        <DetailImage
+          src="https://picsum.photos/seed/picsum/400/500"
+          alt="bear"
+        />
+        <Content>
+          <h2>
+            Ullamco officia qui ipsum excepteur sit sint fugiat in eiusmod ut
+            nisi excepteur id aute lorem consequat aliquip laboris deserunt
+            fugiat esse voluptate quis culpa id et labore sunt veniam
+          </h2>
+          <p>Anim laboris exercitation fugiat, cillum. Fugiat cillum</p>
+        </Content>
+      </Detail>
+      <Detail>
+        <DetailImage
+          src="https://picsum.photos/seed/picsum/400/500"
+          alt="bear"
+        />
+        <Content>
+          <h2>
+            Ullamco officia qui ipsum excepteur sit sint fugiat in eiusmod ut
+            nisi excepteur id aute lorem consequat aliquip laboris deserunt
+            fugiat esse voluptate quis culpa id et labore sunt veniam
+          </h2>
+          <p>
+            Anim laboris exercitation fugiat, cillum. Fugiat cillum, culpa
+            minim. Minim exercitation sint qui deserunt irure proident. Sint qui
+            deserunt irure proident ullamco esse. Deserunt irure proident
+            ullamco esse eiusmod. Proident ullamco, esse eiusmod. Eiusmod aute
+            ad nulla, id. Nulla id qui velit, nulla deserunt quis. Velit, nulla
+            deserunt quis. Quis consequat qui, minim ut. Minim ut proident
+            excepteur consectetur. Proident excepteur consectetur pariatur.
+            Consectetur pariatur exercitation lorem proident enim ut dolore.
+            Exercitation lorem proident enim ut dolore, pariatur. Enim ut dolore
+            pariatur duis enim. Dolore pariatur duis enim, fugiat non esse. Enim
+            fugiat non esse dolor. Non esse dolor, eiusmod sunt. Eiusmod sunt
+            quis, nostrud. Nostrud pariatur aliquip pariatur. Exercitation
+            eiusmod, est nisi. Nisi sunt exercitation ut. Exercitation ut enim
+            sint. Enim sint exercitation, sunt tempor. Sunt tempor laboris
+            cillum eiusmod ex aliqua mollit. Laboris cillum eiusmod ex aliqua
+            mollit aliqua culpa. Eiusmod ex aliqua mollit aliqua culpa officia
+            sunt. Aliqua mollit aliqua, culpa. Eiusmod est nisi sunt. Nisi sunt
+            exercitation ut. Exercitation ut enim sint. Enim sint exercitation,
+            sunt tempor. Sunt tempor laboris cillum eiusmod ex aliqua mollit.
+            Laboris cillum eiusmod ex aliqua mollit aliqua culpa. Eiusmod ex
+            aliqua mollit aliqua culpa officia sunt. Aliqua mollit aliqua,
+            culpa. Culpa officia sunt velit eiusmod laborum. Est nisi sunt
+            exercitation ut enim sint exercitation. Sunt, exercitation ut enim
+            sint exercitation. Enim sint exercitation, sunt tempor. Sunt tempor
+            laboris cillum eiusmod ex aliqua mollit. Laboris cillum eiusmod ex
+            aliqua mollit aliqua culpa. Eiusmod ex aliqua mollit aliqua culpa
+            officia sunt.
+          </p>
+          <p>
+            Anim laboris exercitation fugiat, cillum. Fugiat cillum, culpa
+            minim. Minim exercitation sint qui deserunt irure proident. Sint qui
+            deserunt irure proident ullamco esse. Deserunt irure proident
+            ullamco esse eiusmod. Proident ullamco, esse eiusmod. Eiusmod aute
+            ad nulla, id. Nulla id qui velit, nulla deserunt quis. Velit, nulla
+            deserunt quis. Quis consequat qui, minim ut. Minim ut proident
+            excepteur consectetur. Proident excepteur consectetur pariatur.
+            Consectetur pariatur exercitation lorem proident enim ut dolore.
+            Exercitation lorem proident enim ut dolore, pariatur. Enim ut dolore
+            pariatur duis enim. Dolore pariatur duis enim, fugiat non esse. Enim
+            fugiat non esse dolor. Non esse dolor, eiusmod sunt. Eiusmod sunt
+            quis, nostrud. Nostrud pariatur aliquip pariatur. Exercitation
+            eiusmod, est nisi. Nisi sunt exercitation ut. Exercitation ut enim
+            sint. Enim sint exercitation, sunt tempor. Sunt tempor laboris
+            cillum eiusmod ex aliqua mollit. Laboris cillum eiusmod ex aliqua
+            mollit aliqua culpa. Eiusmod ex aliqua mollit aliqua culpa officia
+            sunt. Aliqua mollit aliqua, culpa. Eiusmod est nisi sunt. Nisi sunt
+            exercitation ut. Exercitation ut enim sint. Enim sint exercitation,
+            sunt tempor. Sunt tempor laboris cillum eiusmod ex aliqua mollit.
+            Laboris cillum eiusmod ex aliqua mollit aliqua culpa. Eiusmod ex
+            aliqua mollit aliqua culpa officia sunt. Aliqua mollit aliqua,
+            culpa. Culpa officia sunt velit eiusmod laborum. Est nisi sunt
+            exercitation ut enim sint exercitation. Sunt, exercitation ut enim
+            sint exercitation. Enim sint exercitation, sunt tempor. Sunt tempor
+            laboris cillum eiusmod ex aliqua mollit. Laboris cillum eiusmod ex
+            aliqua mollit aliqua culpa. Eiusmod ex aliqua mollit aliqua culpa
+            officia sunt.
+          </p>
+          <p>
+            Anim laboris exercitation fugiat, cillum. Fugiat cillum, culpa
+            minim. Minim exercitation sint qui deserunt irure proident. Sint qui
+            deserunt irure proident ullamco esse. Deserunt irure proident
+            ullamco esse eiusmod. Proident ullamco, esse eiusmod. Eiusmod aute
+            ad nulla, id. Nulla id qui velit, nulla deserunt quis. Velit, nulla
+            deserunt quis. Quis consequat qui, minim ut. Minim ut proident
+            excepteur consectetur. Proident excepteur consectetur pariatur.
+            Consectetur pariatur exercitation lorem proident enim ut dolore.
+            Exercitation lorem proident enim ut dolore, pariatur. Enim ut dolore
+            pariatur duis enim. Dolore pariatur duis enim, fugiat non esse. Enim
+            fugiat non esse dolor. Non esse dolor, eiusmod sunt. Eiusmod sunt
+            quis, nostrud. Nostrud pariatur aliquip pariatur. Exercitation
+            eiusmod, est nisi. Nisi sunt exercitation ut. Exercitation ut enim
+            sint. Enim sint exercitation, sunt tempor. Sunt tempor laboris
+            cillum eiusmod ex aliqua mollit. Laboris cillum eiusmod ex aliqua
+            mollit aliqua culpa. Eiusmod ex aliqua mollit aliqua culpa officia
+            sunt. Aliqua mollit aliqua, culpa. Eiusmod est nisi sunt. Nisi sunt
+            exercitation ut. Exercitation ut enim sint. Enim sint exercitation,
+            sunt tempor. Sunt tempor laboris cillum eiusmod ex aliqua mollit.
+            Laboris cillum eiusmod ex aliqua mollit aliqua culpa. Eiusmod ex
+            aliqua mollit aliqua culpa officia sunt. Aliqua mollit aliqua,
+            culpa. Culpa officia sunt velit eiusmod laborum. Est nisi sunt
+            exercitation ut enim sint exercitation. Sunt, exercitation ut enim
+            sint exercitation. Enim sint exercitation, sunt tempor. Sunt tempor
+            laboris cillum eiusmod ex aliqua mollit. Laboris cillum eiusmod ex
+            aliqua mollit aliqua culpa. Eiusmod ex aliqua mollit aliqua culpa
+            officia sunt.
+          </p>
+          <p>
+            Anim laboris exercitation fugiat, cillum. Fugiat cillum, culpa
+            minim. Minim exercitation sint qui deserunt irure proident. Sint qui
+            deserunt irure proident ullamco esse. Deserunt irure proident
+            ullamco esse eiusmod. Proident ullamco, esse eiusmod. Eiusmod aute
+            ad nulla, id. Nulla id qui velit, nulla deserunt quis. Velit, nulla
+            deserunt quis. Quis consequat qui, minim ut. Minim ut proident
+            excepteur consectetur. Proident excepteur consectetur pariatur.
+            Consectetur pariatur exercitation lorem proident enim ut dolore.
+            Exercitation lorem proident enim ut dolore, pariatur. Enim ut dolore
+            pariatur duis enim. Dolore pariatur duis enim, fugiat non esse. Enim
+            fugiat non esse dolor. Non esse dolor, eiusmod sunt. Eiusmod sunt
+            quis, nostrud. Nostrud pariatur aliquip pariatur. Exercitation
+            eiusmod, est nisi. Nisi sunt exercitation ut. Exercitation ut enim
+            sint. Enim sint exercitation, sunt tempor. Sunt tempor laboris
+            cillum eiusmod ex aliqua mollit. Laboris cillum eiusmod ex aliqua
+            mollit aliqua culpa. Eiusmod ex aliqua mollit aliqua culpa officia
+            sunt. Aliqua mollit aliqua, culpa. Eiusmod est nisi sunt. Nisi sunt
+            exercitation ut. Exercitation ut enim sint. Enim sint exercitation,
+            sunt tempor. Sunt tempor laboris cillum eiusmod ex aliqua mollit.
+            Laboris cillum eiusmod ex aliqua mollit aliqua culpa. Eiusmod ex
+            aliqua mollit aliqua culpa officia sunt. Aliqua mollit aliqua,
+            culpa. Culpa officia sunt velit eiusmod laborum. Est nisi sunt
+            exercitation ut enim sint exercitation. Sunt, exercitation ut enim
+            sint exercitation. Enim sint exercitation, sunt tempor. Sunt tempor
+            laboris cillum eiusmod ex aliqua mollit. Laboris cillum eiusmod ex
+            aliqua mollit aliqua culpa. Eiusmod ex aliqua mollit aliqua culpa
+            officia sunt.
+          </p>
+        </Content>
+      </Detail>
+    </DetailedWrapper>
+  );
+}
+
+const DetailedWrapper = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  grid-auto-columns: 1fr;
+  overflow-x: scroll;
+  scroll-snap-type: x mandatory;
+`;
+
+const Detail = styled.div`
+  width: 100vw;
+  scroll-snap-align: center;
+`;
+
+const DetailImage = styled.img`
+  width: 100%;
+`;
+
+const Content = styled.div`
+  padding: 2rem;
+
+  h2 {
+    margin: 0;
+  }
+`;
